891e962062d0117967bedfd72f9d5d031c2e4b41
Maru32768 <maru32768@gmail.com>
2022-11-23 22:58:21 +0900

    init

--- src/main/java/net/kunmc/lab/kamesutaaaaaaa/KamesutaaaaaaaPlugin.java
+++ src/main/java/net/kunmc/lab/kamesutaaaaaaa/KamesutaaaaaaaPlugin.java
@@ -1,8 +1,8 @@
-package net.kunmc.lab.paperplugintemplate;
+package net.kunmc.lab.kamesutaaaaaaa;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PaperPluginTemplate extends JavaPlugin {
+public final class KamesutaaaaaaaPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
     }
